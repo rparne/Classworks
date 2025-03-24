a9baa104e16dc0074c69ff66f431ae9b7474b1fa
rparne <rparne@aum.edu>
2025-03-23 23:29:04 -0500

    Initial Commit

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-const works = {
-    "task1" : {"title" : "Welcome Page", "url" : "works/Task 1.html"},
-    "task2" : {"title" : "Dashboard", "url" : "works/Task 2.html"},
-    "task3" : {"title" : "Hover", "url" : "works/Task 3.html"},
-    "task4" : {"title" : "Welcome to JS", "url" : "works/Task 4.html"},
-    "task5" : {"title" : "Calculator with JS", "url" : "works/Task 5.html"},
-    "task6" : {"title" : "String Operation", "url" : "works/Task 6.html"},
-    "task7" : {"title" : "Grade Calculator", "url" : "works/Task 7.html"},
-    "task8" : {"title" : "Call-Stack with Array ", "url" : "works/Task 8.html"},
-    "task9" : {"title" : "Sum with Loop ", "url" : "works/Task 9.html"},
-    "task10" : {"title" : "While Loop Example ", "url" : "works/Task 11.html"},
-    "task11" : {"title" : "While and Do-While ", "url" : "works/Task 12.html"},
-    "task12" : {"title" : "City Information ", "url" : "works/Task 13.html"},
-    "task13" : {"title" : "City Inventory ", "url" : "works/Task 14.html"},
-    "task14" : {"title" : "Random Number with colors array ", "url" : "works/Task 15.html"},
-};
-
-
 const navDiv = document.getElementById("nav_div");
 const contentFrame = document.getElementById("content_frame");
 
